97c8e7aa2ea3a728839a296215e029acc12cfdab
klassiker <klassiker@lupia.eu>
2021-11-04 10:05:08 +0100

    Add result of training 3

--- python/crypto.py
+++ python/crypto.py
@@ -1,37 +1,93 @@
 from math import floor, sqrt
+import random
 
 
-def add(x,y):
+def add(x, y):
     return x + y
 
-def mul(x,y):
+
+def mul(x, y):
     return x * y
 
-def sub(x,y):
+
+def sub(x, y):
     return x - y
 
-def div(x,y):
+
+def div(x, y):
     return x / y
 
+
+def gen_generator(m: int) -> int:
+    g = None
+
+    if not is_prime(m):
+        return g
+    else:
+        r = random.choice(range(1, m // 4))
+        found = False
+        while not found:
+            g = 2 * r + 1
+            if g < m and is_prime(g):
+                found = True
+            else:
+                r += 1
+
+    return g
+
+
+# trivial algorithm to test conditions of an element
+# being a generator of the group
+def is_generator_simple(g: int, m: int) -> bool:
+    if g >= m:
+        return False
+    elif not is_prime(m):
+        return False
+    elif is_prime(g):
+        return True
+    return False
+
+
+def is_prime(x: int) -> bool:
+    if x < 2:
+        return False
+    if x == 2 or x == 3:
+        return True
+    if x % 2 == 0 or x % 3 == 0:
+        return False
+
+    max_q = floor(sqrt(x))
+    cur_q, step = 5, 2
+
+    while cur_q <= max_q:
+        if x % cur_q == 0:
+            return False
+        cur_q += step
+        step = 6 - step
+
+    return True
+
+
 def factors(x: int) -> [int]:
-        max_q = floor(sqrt(x))
-        cur_q, step = 5, 2
+    max_q = floor(sqrt(x))
+    cur_q, step = 5, 2
 
-        if x % 2 == 0:
-            cur_q = 2
-        elif x % 3 == 0:
-            cur_q = 3
-        else:
-            while cur_q <= max_q:
-                if x % cur_q == 0:
-                    break
-                cur_q += step
-                step = 6 - step
+    if x % 2 == 0:
+        cur_q = 2
+    elif x % 3 == 0:
+        cur_q = 3
+    else:
+        while cur_q <= max_q:
+            if x % cur_q == 0:
+                break
+            cur_q += step
+            step = 6 - step
 
-        if cur_q <= max_q:
-            return [cur_q] + factors(x // cur_q)
-        else:
-            return [x]
+    if cur_q <= max_q:
+        return [cur_q] + factors(x // cur_q)
+    else:
+        return [x]
+
 
 def ext_factors(x):
     primeSet = []
@@ -43,55 +99,58 @@ def ext_factors(x):
             primeSetCount.append(fact.count(y))
     return primeSet, primeSetCount
 
+
 def phi(primes):
     result = 1
     for i in range(len(primes[0])):
-        result *= primes[0][i]**(primes[1][i]-1) * (primes[0][i]-1)
-        
+        result *= primes[0][i] ** (primes[1][i] - 1) * (primes[0][i] - 1)
+
     return result
 
-#Additive group
+
+# Additive group
 class H:
-    def add_mod(self,x,y,m):
+    def add_mod(self, x, y, m):
         return add(x, y) % m
 
-    def add_mod2(self,x,y,m):
+    def add_mod2(self, x, y, m):
         return ((x % m) + (y % m)) % m
 
-    def sub_mod(self,x, y, m):
+    def sub_mod(self, x, y, m):
         return H.add_mod(x, H.add_invert_mod(y, m), m)
 
-    def add_invert_mod(self,x, m):
-        return add(-x,m) %m
-    
-    #skalar multiplication: x=3,y=2, result = x+x = 6
+    def add_invert_mod(self, x, m):
+        return add(-x, m) % m
+
+    # scalar multiplication: x=3,y=2, result = x+x = 6
     def pow_mod(self, x, y, m):
         result = 0
         for i in range(y):
             result = add(result, x)
         return result % m
-    
-    #Order is the number of elements, if H is a group by definition each element is invertable so we count the number of elements
+
+    # Order is the number of elements, if H is a group by definition each element is invertable
+    # so we count the number of elements
     def order(self, m):
         return m
 
-#Multiplikative group
-class G:
-    def mul_mod(self,x,y,m):
-        return mul(x,y) % m
 
-    def mul_mod2(self,x,y,m):
-        return mul(x %m, y %m) %m
-    
+# Multiplicative group
+class G:
+    def mul_mod(self, x, y, m):
+        return mul(x, y) % m
+
+    def mul_mod2(self, x, y, m):
+        return mul(x % m, y % m) % m
+
     def pow_mod(self, x, y, m):
         result = x
-        for i in range(y-1):
+        for i in range(y - 1):
             result = mul(result, x)
         return result % m
-    
+
     def order(self, m):
         return phi(ext_factors(m))
-    
+
     def mul_invert_mod(self, x, m):
-        return x**(self.order(m)-1) % m
-    
+        return x ** (self.order(m) - 1) % m
--- python/testScript.py
+++ python/testScript.py
@@ -1,24 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 28 08:40:48 2021
-
-@author: simon
-"""
-
 import crypto as c
 
 h = c.H()
 g = c.G()
-#print(h.add_mod(2,3,6))
 
-#print(g.pow_mod(2,3,10))
+# print(h.add_mod(2,3,6))
+# print(g.pow_mod(2,3,10))
+# print(c.factors(8))
+# print(c.ext_factors(12))
+# print(c.phi(c.ext_factors(6)))
+# print(g.order(9))
+# print(g.mul_invert_mod(2, 5))
+# print(test.order(mod))
+# print(len(set([test.pow_mod(g, x, mod) for x in range(mod)])))
 
-#print(c.factors(8))
+mod = 877
+order = c.G().order(mod)
+g = c.gen_generator(mod)
 
-#print(c.ext_factors(12))
+print(g)
+q = set([c.G().pow_mod(g, y, mod) for y in range(mod)])
+print(len(q), order, q)
 
-#print(c.phi(c.ext_factors(6)))
+# for x in range(1, mod):
+#     if not c.is_generator_simple(x, mod):
+#         continue
+#     r = pow(x, order, mod)
+#     g = set([c.G().pow_mod(x, y, mod) for y in range(mod)])
+#     if r == 1:
+#         print(x, g)
 
-#print(g.order(9))
+# , [pow(x, y, mod) for y in factors]
+# for mod in range(1,20):
+#     for g in range(2,mod):
+#         if c.is_generator_simple(g, mod):
+#             print(g, mod)
 
-print(g.mul_invert_mod(2, 5))
+
+# for mod in range(20, 50):
+#     generators = [y for y in range(mod) if len(set([test.pow_mod(y, x, mod) for x in range(mod)])) == mod - 1]
+#     if len(generators) > 0:
+#         print(c.factors(mod - 1))
+#         print(mod, generators)
